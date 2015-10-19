79f323a96577de089d9a8347c97d218d6d111ea0
Julien Enocq <julien@enocq.fr>
2015-10-19 13:26:15 +0200

    Call standard destroy and let the plugin call the right method

--- lib/rails-trash.rb
+++ lib/rails-trash.rb
@@ -34,7 +34,7 @@ module Rails
       self.update_attribute(:deleted_at, deleted_at)
       self.class.reflect_on_all_associations(:has_many).each do |reflection|
         if reflection.options[:dependent].eql?(:destroy)
-          self.send(reflection.name).each { |obj| obj.destroy_with_trash }
+          self.send(reflection.name).each { |obj| obj.destroy }
         end
       end
     end
