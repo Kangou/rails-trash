3b24b8d348510bca1e326f8500120902d2ecf2c9
Julien Enocq <julien.enocq@innersense.fr>
2017-08-11 17:16:27 +0200

    fixup

--- lib/rails-trash.rb
+++ lib/rails-trash.rb
@@ -39,12 +39,7 @@ module Rails
       return destroy_without_trash if @trash_is_disabled
       deleted_at = Time.now.utc
       self.update_attribute(:deleted_at, deleted_at)
-      self.class.reflect_on_all_associations(:has_many).each do |reflection|
-        if reflection.options[:dependent].eql?(:destroy)
-          self.send(reflection.name).each { |obj| obj.destroy }
-        end
-      end
-      self.class.reflect_on_all_associations(:has_one).each do |reflection|
+      self.class.reflect_on_all_associations.each do |reflection|
         if reflection.options[:dependent].eql?(:destroy)
           self.send(reflection.name).each { |obj| obj.destroy }
         end
